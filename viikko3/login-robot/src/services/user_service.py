039f7e6dbd4652ffe2756d1c4bf29374a10e9ff4
SJET-Code <santeri.tolonen@helsinki.fi>
2023-11-12 02:05:11 +0200

    viikko3 t3 tehty

--- viikko3/login-robot/src/services/user_service.py
+++ viikko3/login-robot/src/services/user_service.py
@@ -1,5 +1,5 @@
 from entities.user import User
-
+import re
 
 class UserInputError(Exception):
     pass
@@ -37,4 +37,14 @@ class UserService:
         if not username or not password:
             raise UserInputError("Username and password are required")
 
-        # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if not re.match("^[a-z]+$", username):
+            raise UserInputError("Username can only contain letters from a-z")
+        
+        if len(username) < 3:
+            raise UserInputError("Username must be at least 3 characters long")
+
+        if len(password) < 8:
+            raise UserInputError("Password must be at least 8 characters long")
+        
+        if not re.match("^[\S]*[^a-zA-Z]+[\S]*$", password):
+            raise UserInputError("Password must contain non-letter characters")
