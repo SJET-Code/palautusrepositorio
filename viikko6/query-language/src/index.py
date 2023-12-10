adcc73f2cce7a94327a9d61439bffc374f4135a2
SJET-Code <santeri.tolonen@helsinki.fi>
2023-12-10 17:58:03 +0200

    viikko6 t3 tehty

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
@@ -8,13 +8,16 @@ def main():
     stats = Statistics(reader)
 
     matcher = And(
-    HasFewerThan(2, "goals"),
-    PlaysIn("NYR")
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
     for player in stats.matches(matcher):
         print(player)
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
+
 
 if __name__ == "__main__":
     main()
