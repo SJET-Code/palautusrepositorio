4d248d59144f4f7f9078a8866c0bf62ab135a925
SJET-Code <santeri.tolonen@helsinki.fi>
2023-12-10 19:06:57 +0200

    viikko6 t5 tehty

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,20 +1,26 @@
 from matchers import All, And, Or, HasAtLeast, HasFewerThan, PlaysIn, Not
 
 class QueryBuilder:
-    def __init__(self, querry = All()):
-        self.querry = querry
+    def __init__(self, query = All()):
+        self.query = query
 
     def build(self):
-        return self.querry
+        return self.query
     
     def playsIn(self, team):
-        return QueryBuilder(And(self.querry, PlaysIn(team)))
+        return QueryBuilder(And(self.query, PlaysIn(team)))
 
     def hasAtLeast(self, value, attr):
-        return QueryBuilder(And(self.querry, HasAtLeast(value, attr)))
+        return QueryBuilder(And(self.query, HasAtLeast(value, attr)))
 
     def hasFewerThan(self, value, attr):
-        return QueryBuilder(And(self.querry, HasFewerThan(value, attr)))
+        return QueryBuilder(And(self.query, HasFewerThan(value, attr)))
 
     def Not(self, condition):
-        return QueryBuilder(And(self.querry, Not(condition)))
+        return QueryBuilder(And(self.query, Not(condition)))
+
+    def oneOf(*queries):
+        or_query = [queries[1]]
+        for i in range(2,len(queries)):
+            or_query.append(queries[i])
+        return QueryBuilder(Or(*or_query))
